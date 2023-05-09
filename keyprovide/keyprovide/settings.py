6cc567f9c6e730a4f38b17c6bcbf60f381d88ce1
leandro.alves14 <=>
2023-05-08 23:44:17 -0300

    feat: add new table to control donations lists

--- keyprovide/keyprovide/settings.py
+++ keyprovide/keyprovide/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'keyprovide',
         'CLIENT': {
                 'host': 'mongodb+srv://fer:12345@cluster.dpncvxr.mongodb.net/?retryWrites=true&w=majority'
-        }  
+        }
     }
 }
 
